48512ec4d5d224a46757000137fefa8e9fd78c47
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2023-09-24 22:06:05 +0930

    Fixed navbar sticky, better css
    - Removed custom sticky turns out boostrap got u covered
    - addded my own photo
    - Made each section bigger as well as adding padding yay

--- src/navbar.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > navbar.offsetTop) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-});
